98765aa56f1a8b5cbc893c935dac4fc3b3e1c27d
Şeyma Serter <serterseyma254@gmail.com>
2025-01-22 13:51:40 +0300

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: { // firebaseConfig değil, firebase olmalı
+    apiKey: "AIzaSyCVB_ohoQ5zx3V_FYIG__aFDKbxrzscwuc",
+    authDomain: "github-card-clone.firebaseapp.com",
+    projectId: "github-card-clone",
+    storageBucket: "github-card-clone.appspot.com", // Düzeltildi
+    messagingSenderId: "443143841509",
+    appId: "1:443143841509:web:c08db1ad2f0f54fcb3960c",
+    measurementId: "G-1HPFYQ0GXS"
+  }
+};
